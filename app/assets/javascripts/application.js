463eb055bedb7fe69580e5d48b9acc30b9080456
cameronjacoby <cameron.jacoby@gmail.com>
2015-12-12 20:25:51 -0800

    use bootstrap-sass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap.min
+//= require bootstrap-sprockets
 //= require title
 //= require roses_are_red
 //= require violets_are_blue
